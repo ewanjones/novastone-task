71f8d395fa79a338d57a9bfb79db0d3441b372b1
paxthree <ewangjones@btinternet.com>
2018-08-07 10:44:46 +0100

    set up sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from .data.database import init_db, db_session
+from .data.models import User
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+init_db()
+
+#  seed user
+u = User('admin', 'password')
+db_session.add(u)
+db_session.commit()
+
+
+
+@app.route('/signup')
+def signup():
+    return jsonify({'status': 'success'})
+
+@app.route('/users')
+def get_users():
+    print(User.query.all())
+    
+    return jsonify({'status': 'success'})
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
---
+++ data/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    password = Column(String(50), unique=False)
+
+    def __init__(self, name=None, email=None):
+        self.username = username 
+        self.password = password 
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
